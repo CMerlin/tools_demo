bd6d342559ce9a675c4f395b39db72b5ca370ee7
CMerlin <1079843691@qq.com>
2017-12-15 09:49:25 +0800

    1.加入了一个java的测试测试程序

---
+++ 9_JavaDemo/HelloWorld.java
@@ -0,0 +1,12 @@
+/**
+ * brief:这是一个java的hello world程序
+ * 编译:javac HelloWorld.java 得到HelloWorld.class文件
+ * 运行:sudo java HelloWorld
+ * 结果输出：Hello World in Ubuntu!
+ **/
+public class HelloWorld{
+	public static void main(String args[]){
+		System.out.println("Hello World in Ubuntu!");
+		System.out.println("This is my frist code !");
+	}
+}
