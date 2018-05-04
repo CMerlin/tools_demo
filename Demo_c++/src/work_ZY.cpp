bdbe82ebb645af42fe637931dd0f51e951cde9ef
zy <zhangy@gaea.com.cn>
2018-05-04 10:06:39 +0800

    1.WifiDispla获取音视频参数

--- Demo_c++/src/work_ZY.cpp
+++ Demo_c++/src/work_ZY.cpp
@@ -20,4 +20,45 @@ int writeTsData(uint8_t *data, int len){
         return 0;
 }
 
+/************************************************************************************
+* Brief:获取音视频数据参数
+* Out param vformat:获取到的视频参数
+* Out param aformat:获取到的音频参数
+*************************************************************************************/
+int getVideoFromate(char *vformat, char *aformat){
+	int count = 0;
+	char buffer[1024] = {0}, path[64] = {"/data/local/tmp/merlin.file"};
+	FILE *fpin = NULL;
+		
+	fpin = fopen(path, "r");
+	if(NULL == fpin){
+		//printf("[%s]:info=%s inpath=%s LINE:%d\n", __func__, strerror(errno), path, __LINE__);
+		ALOGD("[%s]:info=%s inpath=%s LINE:%d\n", __func__, strerror(errno), path, __LINE__);
+		return -1;
+	}
+
+	if(0 > fgets(buffer, 1024, fpin)){
+		return 0;
+	}
+	char *p = NULL, *saveptr1 = NULL;
+	for(p = strtok_r(buffer, ";", &saveptr1); p != NULL; p = strtok_r(NULL, ";", &saveptr1))
+	{
+		count++;
+		if(NULL == saveptr1){
+			break;
+		}
+		if(1==count){
+			sprintf(vformat, "%s", p);			
+		}else if(2==count){
+			sprintf(aformat, "%s", p);
+		}
+		//printf("[%s][%d]: saveptr1=%s p=%s line:%d\n", __func__, count, saveptr1, p, __LINE__);
+		ALOGD("[%s][%d]: saveptr1=%s p=%s line:%d\n", __func__, count, saveptr1, p, __LINE__);
+	}
+	fclose(fpin);
+	fpin = NULL;
+	
+	return 0;
+}
+
 
